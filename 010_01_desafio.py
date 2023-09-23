8eb0236f7f8bdf561fbdd8fda610042fc23c1adf
Rafael Brito <142283293+rafaelbrittodev@users.noreply.github.com>
2023-09-23 12:35:36 -0300

    Update 010_01_desafio.py

--- 010_01_desafio.py
+++ 010_01_desafio.py
@@ -1,4 +1,4 @@
-'''Escreva um programa que execute o cálculo da Função horária da posição no MRUV,
+'''Escreva um programa que execute o cálculo da método horária da posição no MRUV,
 e retorne de acordo com o tempo informado pelo usuário
 Saída esperada:
 A posição do objeto no tempo x é de y (m)
