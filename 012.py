81d6a9d583b00182be8eee0c2d6548224dd0ebf3
Brian <rafael.silva.brito3@gmail.com>
2023-09-30 10:51:56 -0300

    <build>Exercicies
    Aula 30.09.2023

---
+++ 012.py
@@ -0,0 +1,12 @@
+'''Crie um programa que leia um nome, e mostre o primeiro e o último nome
+Saída esperada:
+Luis Felipe Tatin Vlatkovic
+Primeiro nome: Luis
+Último nome: Vlatkovic
+'''
+Nome = input('Digite seu nome: ').strip()
+print(Nome)
+
+Nome = Nome.split()
+print(f'Primeiro nome: {Nome[0]}')
+print(f'Ultimo nome: {Nome[-1]}')
